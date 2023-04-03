09c25b0109d8f9e1b14fcda057e98e1dd822342d
maxxof <maxim.egorov.soccer@gmail.com>
2023-04-03 18:45:28 +0300

    update index.py

--- tetris-game/src/index.py
+++ tetris-game/src/index.py
@@ -1,11 +1,5 @@
 import pygame
-from button import Button
-from event_queue import EventQueue
-from renderer import Renderer
-from game_loop import GameLoop
 from main_menu import MainMenu
-
-pygame.quit()
 
 def main():
     DISPLAY_WIDTH = 1000
@@ -14,10 +8,6 @@ def main():
     display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
     pygame.display.set_caption("Tetris")
     main_menu = MainMenu(display)
-
-    # event_queue = EventQueue()
-    # renderer = Renderer(display)
-    # game_loop = GameLoop(renderer, event_queue)
 
     pygame.init()
     main_menu.start()
